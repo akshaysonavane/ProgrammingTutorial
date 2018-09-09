5badb0d2b2052503b173f1765ff51eb02330cde0
akshay <akshaysonavane0@gmail.com>
2018-09-09 10:55:05 +0530

    java code examples
    first 4 programs to lern

---
+++ java_t/Relational_003.java
@@ -0,0 +1,30 @@
+public class Math_002
+{
+        public static void main(String[] args)
+
+{                 // inilization of varible 
+        int a=10;
+        int b=5;
+        int Add,Sub,Mul,Div,Que;
+        //arthimathical operation
+        Add=a+b;
+        Sub=a-b;
+        Mul=a*b;
+        Div=a%b;
+        Que=a/b;
+        //Results
+        System.out.println("_________________________________________________");
+        System.out.println("|Relational operator                            |");
+        System.out.println("|_______________________________________________|");
+        System.out.println("|equal to operator \(a==b\)              :" + (a==b) + "         |");
+        System.out.println("|not equal to operator \(a!=b\)          :" + (a!=b) + "         |");
+        System.out.println("|greater than operator \(a>b\)           :" + (a>b) + "          |");
+        System.out.println("|less than operator \(a<b\)              :" + (a<b) + "          |");
+        System.out.println("|greater than operator \(a>=\)           :" + (a>=) + "          |");
+        System.out.println("|     
+        System.out.println("_________________________________________________");
+        }
+}
+
+
+
